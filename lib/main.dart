e060f9033e06df3479eb4706d418e790012390b7
MohammadHIsmail <mhmdhismail95@gmail.com>
2023-01-19 07:57:23 +0200

    full animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:animation/src/app.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 void main() {
+
+  // debugPaintSizeEnabled=true; used to check the positioning and size of widget behavior in app screens
   runApp(const App());
 }
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animation/src/widgets/cat.dart';
 import 'package:flutter/material.dart';
 
@@ -12,19 +14,59 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   Animation<double>? catAnimation;
   AnimationController? catController;
+  Animation<double>? boxAnimation;
+  AnimationController? boxController;
 
   @override
   void initState() {
     catController = AnimationController(
-      duration: const Duration(seconds: 2),
+      duration: const Duration(milliseconds: 500),
       vsync: this
     );
-    catAnimation = Tween(begin: 0.0,end: 100.0)
+    catAnimation = Tween(begin: -35.0,end: -80.0)
     .animate(
       CurvedAnimation(parent: catController!, curve: Curves.easeIn)
     );
-    catController!.forward();
+
+    boxController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this //for the ticker provider mixin
+    );
+    boxAnimation = Tween(begin: pi*0.6,end: pi*0.65) 
+    .animate(
+      CurvedAnimation(
+        parent: boxController!, 
+        curve: Curves.easeInOut //how the tween moves from the begin value to end value 
+      )
+    );
+    boxController!.forward();
+    boxAnimation!.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        boxController!.reverse();
+      }else if(status == AnimationStatus.dismissed){
+        boxController!.forward();
+      }
+    });
+    catAnimation!.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        boxController!.stop();
+      }else if(status == AnimationStatus.dismissed){
+        boxController!.forward();
+      }
+    });
+
     super.initState();
+  }
+
+  onTap(){
+    if(catController!.status == AnimationStatus.completed){
+      // boxController!.forward();
+      catController!.reverse();
+    }else if(catController!.status == AnimationStatus.dismissed){
+      // boxController!.stop();
+      catController!.forward();
+    }
+
   }
 
   @override
@@ -33,20 +75,88 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       appBar: AppBar(
         title: const Text('Animation'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        onTap: onTap,
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              buildCatAnimation(),
+              buildbox(),
+              buildLeftFlap(),
+              buildRightFlap(),
+            ],
+          ),
+        ),
+      )
+      
     );
   }
 
-  Widget buildAnimation(){
+  Widget buildCatAnimation(){
     return AnimatedBuilder(
       animation: catAnimation!, 
       builder: (context, child){
-        return Container(
-          margin: EdgeInsets.only(top: catAnimation!.value),
-          child: child,
+        return Positioned(
+          top: catAnimation!.value,
+          right: 0,
+          left: 0,
+          child: child!,
         );
       },
       child: const Cat(),
+    );
+  }
+
+  Widget buildbox(){
+    return Container(
+      height: 200,
+      width: 200,
+      color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap(){
+    return Positioned(
+      left: 3,
+      top: 2,
+      child: AnimatedBuilder(
+        animation: boxAnimation!,
+        builder: (context, child){
+          return Transform.rotate(
+            alignment: Alignment.topLeft,
+            angle: boxAnimation!.value,
+            child: child
+          );
+        },
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRightFlap(){
+    return Positioned(
+      right: 3,
+      top: 2,
+      child: AnimatedBuilder(
+        animation: boxAnimation!,
+        builder: (context, child){
+          return Transform.rotate(
+            alignment: Alignment.topRight,
+            angle: -boxAnimation!.value,
+            child: child
+          );
+        },
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+      ),
     );
   }
 }
